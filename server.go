f9e8d1dc7e30e9da903d8afb86b44f79db5a5d71
Robert Lehmann <lehmrob@gmail.com>
2017-05-13 20:51:50 +0200

    server: Add run to invoke ssh server

--- server.go
+++ server.go
@@ -1,11 +1,26 @@
 package remotectrl
 
+import (
+	"fmt"
+	"io"
+
+	"github.com/gliderlabs/ssh"
+)
+
 // ServerConfig holds information for the remotectrl server
-type ServerConfig struct {
+type Server struct {
 	// Port is the port which will be used fot the server to listen
 	Port string
 	// User for the registration on the remotectrl server
 	User string
 	// Pass is the password phrase for the user to login on the server
 	Pass string
+}
+
+func (s *Server) Run() error {
+	ssh.Handle(func(s ssh.Session) {
+		io.WriteString(s, fmt.Sprintf("Hello World %s\n", s.User()))
+	})
+
+	return ssh.ListenAndServe(s.Port, nil)
 }
