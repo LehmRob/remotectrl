233afcb625a4a26a5a05c124fd564777e543f8d8
Robert Lehmann <lehmrob@gmail.com>
2017-05-13 23:03:18 +0200

    WIP: Update server and main file

--- cmd/remotectrld/main.go
+++ cmd/remotectrld/main.go
@@ -8,18 +8,34 @@ import (
 )
 
 var (
-	port = flag.String("port", ":2020", "Port for the server for listening")
-	user = flag.String("user", "user", "User for the server to register")
-	pass = flag.String("pass", "secret", "Password which is needed to register on the server")
+	port   = flag.String("port", ":2020", "Port for the server for listening")
+	user   = flag.String("user", "user", "User for the server to register")
+	pass   = flag.String("pass", "secret", "Password which is needed to register on the server")
+	server = flag.Bool("server", false, "Flag indicates if the daemon should work in server mode or not")
+	host   = flag.String("host", "", "Host is the host address for the server, use this flag only in client mode (no -server flag)")
 )
 
 func main() {
 	log.Println("Hello World")
 	flag.Parse()
 
-	server := remotectrl.Server{
-		Port: *port,
-	}
+	if *server {
+		log.Println("Starting Server")
+		s := remotectrl.Server{
+			Port: *port,
+			User: *user,
+			Pass: *pass,
+		}
 
-	log.Fatal(server.Run())
+		log.Fatal(s.Run())
+	} else {
+		log.Println("Starting Client")
+		c := remotectrl.Client{
+			Host: *host,
+			Port: *port,
+			User: *user,
+			Pass: *pass,
+		}
+		log.Fatal(c.Run())
+	}
 }
--- server.go
+++ server.go
@@ -1,13 +1,17 @@
+// Copyright (c) 2017 Robert Lehmann <lehmrob@gmail.com>
+// This software is licensed under BSD Clause 2 License
+// Look into the LICENSE file for more information
+
 package remotectrl
 
 import (
-	"fmt"
 	"io"
+	"os"
 
 	"github.com/gliderlabs/ssh"
 )
 
-// ServerConfig holds information for the remotectrl server
+// Server holds information for the remotectrl server
 type Server struct {
 	// Port is the port which will be used fot the server to listen
 	Port string
@@ -15,12 +19,24 @@ type Server struct {
 	User string
 	// Pass is the password phrase for the user to login on the server
 	Pass string
+
+	server ssh.Server
 }
 
 func (s *Server) Run() error {
-	ssh.Handle(func(s ssh.Session) {
-		io.WriteString(s, fmt.Sprintf("Hello World %s\n", s.User()))
+	s.server = ssh.Server{
+		Addr: s.Port,
+		PasswordHandler: func(ctx ssh.Context, pass string) bool {
+			return ctx.User() == s.User && pass == s.Pass
+		},
+	}
+
+	s.server.Handle(func(s ssh.Session) {
+		for {
+			go io.Copy(s, os.Stdout)
+			io.Copy(os.Stdin, s)
+		}
 	})
 
-	return ssh.ListenAndServe(s.Port, nil)
+	return s.server.ListenAndServe()
 }
