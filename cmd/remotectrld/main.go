6f8530e6d20ec13b0f531a3993450aa4e183dbce
Robert Lehmann <lehmrob@gmail.com>
2017-05-13 20:52:20 +0200

    cmd:remotectrld: Starting server with port

--- cmd/remotectrld/main.go
+++ cmd/remotectrld/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"flag"
 	"log"
+
+	"github.com/lehmrob/remotectrl"
 )
 
 var (
-	server = flag.String("port", ":2020", "Port for the server for listening")
-	user   = flag.String("user", "user", "User for the server to register")
-	pass   = flag.String("pass", "secret", "Password which is needed to register on the server")
+	port = flag.String("port", ":2020", "Port for the server for listening")
+	user = flag.String("user", "user", "User for the server to register")
+	pass = flag.String("pass", "secret", "Password which is needed to register on the server")
 )
 
 func main() {
 	log.Println("Hello World")
 	flag.Parse()
+
+	server := remotectrl.Server{
+		Port: *port,
+	}
+
+	log.Fatal(server.Run())
 }
